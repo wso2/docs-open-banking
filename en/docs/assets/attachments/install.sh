d685e3a05e12ceac96f781900ec725962d19b9b9
isharailanga <isharailanga129@gmail.com>
2021-06-21 17:00:22 +0530

    Update install.sh script

--- en/docs/assets/attachments/install.sh
+++ en/docs/assets/attachments/install.sh
@@ -1,27 +1,27 @@
-WSO2_OB_APIM_HOME=$1
+PRODUCT_HOME=$1
 
 
 
 # set product home
-if [ "${WSO2_OB_APIM_HOME}" == "" ];
+if [ "${PRODUCT_HOME}" == "" ];
   then
     
-    WSO2_OB_APIM_HOME=$(pwd)
-    echo "Product home is: ${WSO2_OB_APIM_HOME}"
+    PRODUCT_HOME=$(pwd)
+    echo "Product home is: ${PRODUCT_HOME}"
 fi
 
 # validate product home
-if [ ! -d "${WSO2_OB_APIM_HOME}/repository/components" ]; then
+if [ ! -d "${PRODUCT_HOME}/repository/components" ]; then
   echo -e "\n\aERROR:specified product path is not a valid carbon product path\n";
   exit 2;
 else
   echo -e "\nValid carbon product path.\n";
 fi
 
-find "${WSO2_OB_APIM_HOME}"/repository/components/dropins -name "com.wso2.*" -exec mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile={} \;
-find "${WSO2_OB_APIM_HOME}"/repository/components/lib -name "com.wso2.*" -exec mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile={} \;
-find "${WSO2_OB_APIM_HOME}"/repository/components/plugins -name "com.wso2.*" -exec mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile={} \;
+find "${PRODUCT_HOME}"/repository/components/dropins -name "com.wso2.*" -exec mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile={} \;
+find "${PRODUCT_HOME}"/repository/components/lib -name "com.wso2.*" -exec mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile={} \;
+find "${PRODUCT_HOME}"/repository/components/plugins -name "com.wso2.*" -exec mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile={} \;
 
-find "${WSO2_OB_APIM_HOME}"/repository/components/dropins -name "org.wso2.*" -exec mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile={} \;
-find "${WSO2_OB_APIM_HOME}"/repository/components/lib -name "org.wso2.*" -exec mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile={} \;
-find "${WSO2_OB_APIM_HOME}"/repository/components/plugins -name "org.wso2.*" -exec mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile={} \;
+find "${PRODUCT_HOME}"/repository/components/dropins -name "org.wso2.*" -exec mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile={} \;
+find "${PRODUCT_HOME}"/repository/components/lib -name "org.wso2.*" -exec mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile={} \;
+find "${PRODUCT_HOME}"/repository/components/plugins -name "org.wso2.*" -exec mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile={} \;
